447955013c55747a86c9f2afb4b7a2ade56db0cf
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2020-08-11 05:03:01 +0100

    add rough output of the site map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello Web Crawler");
+import { generateSiteMetadata } from "./webCrawler";
+import formatSiteMap from "./siteMapFormatter";
+
+const printMetadata = async () => {
+  const metadata = await generateSiteMetadata(url);
+  console.log(formatSiteMap(metadata.siteMap));
+};
+
+const url = process.argv[2];
+
+printMetadata(url);
