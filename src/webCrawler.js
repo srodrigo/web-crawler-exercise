6a0eca436b711638dffabcc0454bef4381ac7648
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2020-08-11 09:34:37 +0100

    refactor: move functions to outer scope

--- src/webCrawler.js
+++ src/webCrawler.js
@@ -1,95 +1,93 @@
 import axios from "axios";
 import { JSDOM } from "jsdom";
 
-const generateSiteMetadata = async (rootUrl, logger = console) => {
-  const domain = new URL(rootUrl).hostname;
+const isRelativePath = url => /^\/[\w\d\-]+/g.test(url);
+const isValidUrl = url => /(^http[s]?:\/{2})|(^www)|(^\/{1,2}[\w\d\-]+)/g.test(url);
 
-  const isRelativePath = url => /^\/[\w\d\-]+/g.test(url);
-  const isValidUrl = url => /(^http[s]?:\/{2})|(^www)|(^\/{1,2}[\w\d\-]+)/g.test(url);
+const createNode = (url, children = []) => ({
+  url,
+  children,
+});
+
+const findNodeWithUrl = (metadata, url) => {
+  if (metadata.url === url) {
+    return metadata;
+  }
+
+  const children = [...metadata.children];
+  while (children.length > 0) {
+    const child = children.shift();
+    if (child.url === url) {
+      return child;
+    }
+
+    child.children.forEach(grandChild => {
+      children.push(grandChild);
+    });
+  }
+
+  return null;
+};
+
+const generatePageMetadata = async (rootUrl, logger) => {
+  const pageMetadata = createNode(rootUrl);
+
+  const domain = new URL(rootUrl).hostname;
+  const visitedPages = [];
 
   const addHostname = url =>
     url.includes(domain) || !isRelativePath(url) ? url : `${rootUrl}${url}`;
 
-  const findNodeWithUrl = (metadata, url) => {
-    if (metadata.url === url) {
-      return metadata;
-    }
+  const urlsToFetch = [addHostname(rootUrl)];
 
-    const children = [...metadata.children];
-    while (children.length > 0) {
-      const child = children.shift();
-      if (child.url === url) {
-        return child;
-      }
-
-      child.children.forEach(grandChild => {
-        children.push(grandChild);
-      });
-    }
-
-    return null;
-  };
-
-  const createNode = (url, children = []) => ({
-    url,
-    children,
-  });
-
-  const generatePageMetadata = async () => {
-    const pageMetadata = createNode(rootUrl);
-
-    const visitedPages = [];
-    const urlsToFetch = [addHostname(rootUrl)];
-
-    const processLinks = (document, parent) => {
-      [...document.querySelectorAll("a[href], link[href]")]
-        .map(link => link.getAttribute("href"))
-        .filter(link => isValidUrl(link))
-        .map(addHostname)
-        .forEach(fullLinkUrl => {
-          if (
-            !visitedPages.includes(fullLinkUrl) &&
-            !urlsToFetch.includes(fullLinkUrl) &&
-            isValidUrl(fullLinkUrl)
-          ) {
-            parent.children.push(createNode(fullLinkUrl));
-            if (fullLinkUrl.includes(domain)) {
-              urlsToFetch.push(fullLinkUrl);
-            }
+  const processLinks = (document, parent) => {
+    [...document.querySelectorAll("a[href], link[href]")]
+      .map(link => link.getAttribute("href"))
+      .filter(link => isValidUrl(link))
+      .map(addHostname)
+      .forEach(fullLinkUrl => {
+        if (
+          !visitedPages.includes(fullLinkUrl) &&
+          !urlsToFetch.includes(fullLinkUrl) &&
+          isValidUrl(fullLinkUrl)
+        ) {
+          parent.children.push(createNode(fullLinkUrl));
+          if (fullLinkUrl.includes(domain)) {
+            urlsToFetch.push(fullLinkUrl);
           }
-        });
-    };
+        }
+      });
+  };
 
-    const processScripts = (document, parent) => {
-      [...document.querySelectorAll("script[src]")]
-        .map(script => script.getAttribute("src"))
-        .forEach(link => parent.children.push(createNode(link)));
-    };
+  const processScripts = (document, parent) => {
+    [...document.querySelectorAll("script[src]")]
+      .map(script => script.getAttribute("src"))
+      .forEach(link => parent.children.push(createNode(link)));
+  };
 
-    while (urlsToFetch.length > 0) {
-      const url = urlsToFetch.shift();
-      visitedPages.push(url);
+  while (urlsToFetch.length > 0) {
+    const url = urlsToFetch.shift();
+    visitedPages.push(url);
 
-      try {
-        logger.log(`Fetching ${url}`);
-        const { data } = await axios.get(url);
-        const dom = new JSDOM(data);
-        const { document } = dom.window;
+    try {
+      logger.log(`Fetching ${url}`);
+      const { data } = await axios.get(url);
+      const dom = new JSDOM(data);
+      const { document } = dom.window;
 
-        const parent = findNodeWithUrl(pageMetadata, url);
-        processLinks(document, parent);
-        processScripts(document, parent);
-      } catch (error) {
-        logger.log(`Could not fetch ${url}`);
-      }
+      const parent = findNodeWithUrl(pageMetadata, url);
+      processLinks(document, parent);
+      processScripts(document, parent);
+    } catch (error) {
+      logger.log(`Could not fetch ${url}`);
     }
+  }
 
-    return pageMetadata;
-  };
-
-  return {
-    siteMap: await generatePageMetadata(),
-  };
+  return pageMetadata;
 };
+
+const generateSiteMetadata = async (rootUrl, logger = console) => ({
+  siteMap: await generatePageMetadata(rootUrl, logger),
+});
 
 export { generateSiteMetadata };
